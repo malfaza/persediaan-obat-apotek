fd4b9101d632de5b60893f7595e1885e633ab206
malfaza <109192691+malfaza@users.noreply.github.com>
2023-12-31 13:30:13 +0700

    Add files via upload

---
+++ database/migrations/2023_10_21_044555_create_supplier_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSuppliersTable extends Migration {
+    public function up() {
+        Schema::create('suppliers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down() {
+        Schema::dropIfExists('suppliers');
+    }
+}
+
